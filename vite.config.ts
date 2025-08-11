4b76aa092a302ffbb9286ccc3f33fc16b9248e52
Lalu Immaratul Ardhi Ganeru <immaratulganeru@gmail.com>
2025-08-11 14:47:03 +0700

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: '10.7.146.114',
+            host: '10.7.1.253',
             port: 5173,
         },
         watch: {
