ce13716b45770872d3e13d64be041710033023d3
Lalu Immaratul Ardhi Ganeru <immaratulganeru@gmail.com>
2025-12-15 10:19:35 +0700

    removed rounding for manual input

--- app/Models/TbInput.php
+++ app/Models/TbInput.php
@@ -7,19 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class TbInput extends Model
 {
     protected $table = 'tb_input';
-    
+
     // Since the table doesn't follow Laravel's naming convention for timestamps
     public $timestamps = false;
-    
+
     protected $fillable = [
         'tag_no',
-        'value', 
+        'value',
         'date_rec',
         'perf_id'
     ];
-    
+
     protected $casts = [
         'date_rec' => 'datetime',
-        'value' => 'decimal:2'
+        'value' => 'float'
     ];
-} 
+}
